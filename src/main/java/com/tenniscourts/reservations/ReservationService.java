249c4c951ec5a1382b7ef519349d9f10e719fdd1
jonascc <jonasccarmo@gmail.com>
2021-10-06 22:29:15 -0300

    Implement missing method and fix rescheduleReservation()

--- src/main/java/com/tenniscourts/reservations/ReservationService.java
+++ src/main/java/com/tenniscourts/reservations/ReservationService.java
@@ -1,6 +1,12 @@
 package com.tenniscourts.reservations;
 
 import com.tenniscourts.exceptions.EntityNotFoundException;
+import com.tenniscourts.guests.Guest;
+import com.tenniscourts.guests.GuestMapper;
+import com.tenniscourts.guests.GuestService;
+import com.tenniscourts.schedules.Schedule;
+import com.tenniscourts.schedules.ScheduleMapper;
+import com.tenniscourts.schedules.ScheduleService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -16,8 +22,25 @@ public class ReservationService {
 
     private final ReservationMapper reservationMapper;
 
+    private final GuestService guestService;
+
+    private final GuestMapper guestMapper;
+
+    private final ScheduleService scheduleService;
+
+    private final ScheduleMapper scheduleMapper;
+
+    private final BigDecimal RESERVATION_FEE = new BigDecimal(10);
+
     public ReservationDTO bookReservation(CreateReservationRequestDTO createReservationRequestDTO) {
-        throw new UnsupportedOperationException();
+        Guest guest = guestMapper.map(guestService.findGuestById(createReservationRequestDTO.getGuestId()));
+        Schedule schedule = scheduleMapper.map(scheduleService.findSchedule(createReservationRequestDTO.getScheduleId()));
+        if (schedule.getStartDateTime().isBefore(LocalDateTime.now())) {
+            throw new IllegalArgumentException("Can schedule only future dates.");
+        }
+        Reservation reservation = reservationMapper.map(createReservationRequestDTO);
+        reservation.setValue(RESERVATION_FEE);
+        return reservationMapper.map(reservationRepository.saveAndFlush(reservation));
     }
 
     public ReservationDTO findReservation(Long reservationId) {
@@ -63,9 +86,16 @@ public class ReservationService {
 
     public BigDecimal getRefundValue(Reservation reservation) {
         long hours = ChronoUnit.HOURS.between(LocalDateTime.now(), reservation.getSchedule().getStartDateTime());
+        long minutes = ChronoUnit.MINUTES.between(LocalDateTime.now(), reservation.getSchedule().getStartDateTime());
 
         if (hours >= 24) {
             return reservation.getValue();
+        } else if (hours >= 12 && hours < 24) {
+            return reservation.getValue().multiply(new BigDecimal("0.75"));
+        } else if (hours >= 2 && hours < 12) {
+            return reservation.getValue().multiply(new BigDecimal("0.5"));
+        } else if (minutes > 0 && hours < 2) {
+            return reservation.getValue().multiply(new BigDecimal("0.25"));
         }
 
         return BigDecimal.ZERO;
@@ -74,14 +104,16 @@ public class ReservationService {
     /*TODO: This method actually not fully working, find a way to fix the issue when it's throwing the error:
             "Cannot reschedule to the same slot.*/
     public ReservationDTO rescheduleReservation(Long previousReservationId, Long scheduleId) {
-        Reservation previousReservation = cancel(previousReservationId);
+        Reservation previousReservation = reservationMapper.map(findReservation(previousReservationId));
 
         if (scheduleId.equals(previousReservation.getSchedule().getId())) {
             throw new IllegalArgumentException("Cannot reschedule to the same slot.");
         }
 
-        previousReservation.setReservationStatus(ReservationStatus.RESCHEDULED);
-        reservationRepository.save(previousReservation);
+        this.validateCancellation(previousReservation);
+
+        BigDecimal refundValue = getRefundValue(previousReservation);
+        updateReservation(previousReservation, refundValue, ReservationStatus.RESCHEDULED);
 
         ReservationDTO newReservation = bookReservation(CreateReservationRequestDTO.builder()
                 .guestId(previousReservation.getGuest().getId())
@@ -90,4 +122,5 @@ public class ReservationService {
         newReservation.setPreviousReservation(reservationMapper.map(previousReservation));
         return newReservation;
     }
+
 }
